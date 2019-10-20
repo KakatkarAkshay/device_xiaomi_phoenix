19a722606395563bdbea7227ae7e629c9c8ee475
Ethan Chen <intervigil@gmail.com>
2019-10-20 01:17:32 -0700

    phoenix: Add AOSP RCS packages

--- device.mk
+++ device.mk
@@ -133,6 +133,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.phoenix
 
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
