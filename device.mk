2febd1e38d628aaa61855b80b6046e558726836a
Akshay Kakatkar <cryllicbuster273@pixelexperience.org>
2020-11-21 18:18:42 +0100

    phoenix: power: Rebase on LA.UM.9.1.r1.07700-SMxxx0.0

--- device.mk
+++ device.mk
@@ -134,7 +134,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.phoenix
+    android.hardware.power-service.phoenix
 
 # Telephony
 PRODUCT_PACKAGES += \
